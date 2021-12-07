59d2f9d8eba420e9871a1014a5adc238556c9f42
romaklimenko <romaklimenko@gmail.com>
2021-12-07 07:59:05 +0100

    notes

--- src/notes.ts
+++ src/notes.ts
@@ -2,12 +2,17 @@ import {get} from './api';
 import {NotesResponse} from './types';
 
 export async function note(fromUserName, toUserName) {
-  const response = await get(
-    `user_notes/?user_login=${toUserName}&author_login=${fromUserName}`
-  );
-  const notes = (await response.json()) as NotesResponse;
-  if (notes.item_count > 0) {
-    return notes.user_notes[0];
+  try {
+    const response = await get(
+      `user_notes/?user_login=${toUserName}&author_login=${fromUserName}`
+    );
+    const notes = (await response.json()) as NotesResponse;
+    if (notes.item_count > 0) {
+      return notes.user_notes[0];
+    }    
+  } catch (error) {
+    console.error(error);
   }
+
   return null;
 }
